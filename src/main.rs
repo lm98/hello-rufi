104472cd18cdc1696ba2f2b2f0fd228560849d48
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-10-17 16:49:30 +0200

    chore: add comments

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,12 @@ use hello_rufi::gradient::gradient;
 
 fn main() {
     let devices = vec![1, 2, 3, 4, 5];
+    /* Set up a simple topology that will be used for these tests.
+    *  Topology: [1] -- [2] -- [3] -- [4] -- [5].
+    */
     let mut states: HashMap<i32, DeviceState> = devices.iter().map(|d|{
         let nbrs: Vec<i32> = vec![d.clone()-1, d.clone(), d.clone()+1].into_iter().filter(|n| (n > &0 && n < &6)).collect();
+        // In this example, we set the source to be device 2.
         let local_sensor: HashMap<SensorId, Rc<Box<dyn Any>>> = vec![(sensor("source"), Rc::new(Box::new(*d == 2) as Box<dyn Any>))].into_iter().collect();
         let nbr_sensor: HashMap<SensorId, HashMap<i32, Rc<Box<dyn Any>>>> = HashMap::from([(sensor("nbr_range"), nbrs.iter().map(|n| (n.clone(), Rc::new(Box::new(i32::abs(d - n)) as Box<dyn Any>))).collect())]);
         let state = DeviceState{
@@ -29,20 +33,25 @@ fn main() {
 
     let scheduling: Vec<i32> = iter::repeat(devices).take(5).flatten().collect();
 
+    // For each device in the provided scheduling, run the program on the device.
     for d in scheduling {
         let curr = states.get(&d).unwrap().clone();
         let ctx = Context::new(d, curr.local_sensor, curr.nbr_sensor, curr.exports);
         println!("RUN: DEVICE {}\n\tCONTEXT {:?}", d, ctx);
+        // Setup the VM
         let mut vm = RoundVM::new(ctx);
         vm.export_stack.push(Export::new());
+        // Run the program
         let (mut vm_, res) = round(vm, gradient);
         let mut to_update = states.get(&d).unwrap().clone();
         to_update.update_exports(d, vm_.export_data().clone());
+        // Update the exports of the neighbors, simulating the message passing
         to_update.nbr_sensor.get(&sensor("nbr_range")).unwrap().keys().for_each(|nbr| {
             let mut nbr_state = states.get(nbr).unwrap().clone();
             nbr_state.update_exports(d, to_update.exports.get(&d).unwrap().clone());
             states.insert(nbr.clone(), nbr_state);
         });
+        // Update the topology with the new state
         states.insert(d, to_update);
         println!("\t EXPORT: {:?}\n\t OUTPUT: {:?}\n\t", states.get(&d).unwrap().exports.get(&d).unwrap(), res);
     }
