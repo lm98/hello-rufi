dfc927763fe290506ea2aa8224af0fd12289c053
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-10-31 10:33:04 +0100

    chore: remove useless files

--- distributed-gradient/src/network.rs
+++
@@ -1,9 +0,0 @@
-use std::collections::HashMap;
-use crate::message::Message;
-
-mod implements;
-
-pub trait Network {
-    fn send(&self, self_id: i32, data: Message);
-    fn receive(&self) -> HashMap<i32, Message>;
-}
