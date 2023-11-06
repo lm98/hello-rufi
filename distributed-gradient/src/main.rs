4ca3cb77943479d772079c1295eb34bdac780377
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-11-06 10:22:41 +0100

    chore: add more comments

--- distributed-gradient/src/main.rs
+++ distributed-gradient/src/main.rs
@@ -14,24 +14,27 @@ use rufi_gradient::gradient;
 use rumqttc::{AsyncClient, MqttOptions, QoS};
 use distributed_gradient::message::Message;
 
+// This enum represent the different command we will send between channels
 #[derive(Debug)]
 enum Command {
-    Get,
     Send {
         msg: String,
     }
 }
 
+
+
 #[tokio::main]
 async fn main() {
-    let mut args: Vec<String> = std::env::args().collect();
+    // Get arguments from the CLI
+    let args: Vec<String> = std::env::args().collect();
     let self_id: i32 = i32::from_str(&args[1]).unwrap();
 
     // Setup the MQTT client
     let mut mqttoptions = MqttOptions::new(format!("device#{}", self_id), "test.mosquitto.org", 1883);
     mqttoptions.set_keep_alive(Duration::from_secs(5));
-
     let (client, mut eventloop) = AsyncClient::new(mqttoptions, 20);
+
     /* Set up a simple topology that will be used for these tests.
     *  Topology: [1] -- [2] -- [3] -- [4] -- [5].
     */
@@ -39,10 +42,9 @@ async fn main() {
     let local_sensor: HashMap<SensorId, Rc<Box<dyn Any>>> = vec![(sensor("source"), Rc::new(Box::new(self_id == 2) as Box<dyn Any>))].into_iter().collect();
     let nbr_sensor: HashMap<SensorId, HashMap<i32, Rc<Box<dyn Any>>>> = HashMap::from([(sensor("nbr_range"), nbrs.iter().map(|n| (n.clone(), Rc::new(Box::new(i32::abs(self_id - n)) as Box<dyn Any>))).collect())]);
 
+    // Spawn a task to handle the messages sent from neighbors
     let client_clone = client.clone();
     let (tx, mut rx) = mpsc::channel::<Command>(32);
-
-    // Spawn a task to handle the messages sent from neighbors
     tokio::spawn(async move {
         subscriptions(client_clone, nbrs).await;
         // poll the eventloop for new messages
@@ -55,6 +57,7 @@ async fn main() {
         }
     });
 
+    // Setup the aggregate program execution
     let mut states: HashMap<i32, Export> = HashMap::new();
     loop {
         //Execute a round
@@ -66,11 +69,13 @@ async fn main() {
         let self_export: Export = vm_.export_data().clone();
         states.insert(self_id, self_export.clone());
         println!("OUTPUT: {}\nEXPORT: {}\n", result, self_export);
+
         // Publish the export
         let msg = Message {  source: self_id, export: self_export.clone() };
         let msg_ser = serde_json::to_string(&msg).unwrap();
         client.publish(format!("hello-rufi/{self_id}/subscriptions"), QoS::AtMostOnce, false, Bytes::from(msg_ser)).await.unwrap();
-        // Receive the export from the message task
+
+        // Receive the neighbouring exports from the message task
         if let Some(message) = rx.recv().await {
             if let Command::Send { msg } = message {
                 let msg: Message = serde_json::from_str(&msg).unwrap();
