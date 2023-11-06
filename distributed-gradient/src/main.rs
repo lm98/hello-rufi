1c79e7336fa5c6a8c5fcebf766a18439f3296d1f
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-11-06 10:57:27 +0100

    feat: add the possibility to specify if the node is source from cli

--- distributed-gradient/src/main.rs
+++ distributed-gradient/src/main.rs
@@ -22,13 +22,33 @@ enum Command {
     }
 }
 
+#[derive(Debug, Default)]
+struct Arguments {
+    pub id: i32,
+    pub source: bool,
+}
 
+impl Arguments {
+    pub fn parse<S: AsRef<str>>(args: impl IntoIterator<Item=S>) -> Result<Arguments, String> {
+        let mut r = Arguments::default();
+
+        for arg in args {
+            match arg.as_ref() {
+                "-t" => r.source = true,
+                x => r.id = x.parse::<i32>().unwrap(),
+            }
+        }
+
+        Ok(r)
+    }
+}
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), String> {
     // Get arguments from the CLI
-    let args: Vec<String> = std::env::args().collect();
-    let self_id: i32 = i32::from_str(&args[1]).unwrap();
+    let args = Arguments::parse(std::env::args().skip(1))?;
+    let self_id = args.id;
+    let is_source = args.source;
 
     // Setup the MQTT client
     let mut mqttoptions = MqttOptions::new(format!("device#{}", self_id), "test.mosquitto.org", 1883);
@@ -39,7 +59,7 @@ async fn main() {
     *  Topology: [1] -- [2] -- [3] -- [4] -- [5].
     */
     let nbrs: Vec<i32> = vec![self_id.clone()-1, self_id.clone(), self_id.clone()+1].into_iter().filter(|n| (n > &0 && n < &6)).collect();
-    let local_sensor: HashMap<SensorId, Rc<Box<dyn Any>>> = vec![(sensor("source"), Rc::new(Box::new(self_id == 2) as Box<dyn Any>))].into_iter().collect();
+    let local_sensor: HashMap<SensorId, Rc<Box<dyn Any>>> = vec![(sensor("source"), Rc::new(Box::new(is_source) as Box<dyn Any>))].into_iter().collect();
     let nbr_sensor: HashMap<SensorId, HashMap<i32, Rc<Box<dyn Any>>>> = HashMap::from([(sensor("nbr_range"), nbrs.iter().map(|n| (n.clone(), Rc::new(Box::new(i32::abs(self_id - n)) as Box<dyn Any>))).collect())]);
 
     // Spawn a task to handle the messages sent from neighbors
@@ -84,6 +104,7 @@ async fn main() {
         }
         tokio::time::sleep(Duration::from_secs(2)).await;
     }
+
 }
 
 async fn subscriptions(client: AsyncClient, nbrs: Vec<i32>) {
