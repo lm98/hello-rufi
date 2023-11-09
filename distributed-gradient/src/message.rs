47cd6288ef3649ad2092f97e442fc0bcf05b247a
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-11-09 10:27:18 +0100

    chore: add comments

--- distributed-gradient/src/message.rs
+++ distributed-gradient/src/message.rs
@@ -1,6 +1,7 @@
 use rf_core::export::Export;
 use serde::{Deserialize, Serialize};
 
+/// This struct represent a message that will be sent between nodes.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Message {
     pub source: i32,
