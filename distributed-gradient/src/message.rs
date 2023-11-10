1f986ea1499cd2f8468426ceb7e0c08ac5783dd4
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-11-10 13:55:50 +0100

    feat: add message queue

--- distributed-gradient/src/message.rs
+++ distributed-gradient/src/message.rs
@@ -1,9 +1,37 @@
 use rf_core::export::Export;
 use serde::{Deserialize, Serialize};
 
+pub mod message_queue;
+
 /// This struct represent a message that will be sent between nodes.
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq,Serialize, Deserialize)]
 pub struct Message {
     pub source: i32,
     pub export: Export,
+}
+
+impl Message {
+    pub fn new(source: i32, p1: Export) -> Self {
+        Self {
+            source,
+            export: p1,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::any::Any;
+    use std::collections::HashMap;
+    use rf_core::path::Path;
+    use rf_core::export;
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let export = export!((Path::new(), 1));
+        let msg = super::Message::new(1, export.clone());
+        assert_eq!(msg.source, 1);
+        assert_eq!(msg.export, export);
+    }
 }
